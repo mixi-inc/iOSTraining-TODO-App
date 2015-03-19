cac9ebf3ef3cdd98599f5c133d643a69cb0c370e
Yuichi.Takeda <yuichi.takeda@mixi.co.jp>
2015-03-19 20:40:18 +0900

    TODOをtableViewに表示できるようにした

--- Todo/ViewController.m
+++ Todo/ViewController.m
@@ -8,9 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource>
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
-
+@property (strong, nonatomic) NSArray *todo;
 @end
 
 @implementation ViewController
@@ -18,11 +18,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.todo = @[@"牛乳を買ってくる",
+                  @"ビールを飲む",
+                  @"家賃の振り込み",
+                  @"洗剤を買い足す",
+                  @"Macのアップデート",
+                  @"ルンバの充電",
+                  @"結婚式の招待状に返信する",
+                  @"犬の散歩",
+                  @"雨ニモマケズ 風ニモマケズ 雪ニモ夏ノ暑サニモマケヌ 丈夫ナカラダヲモチ 慾ハナク 決シテ瞋ラズ イツモシヅカニワラッテヰル 一日ニ玄米四合ト 味噌ト少シノ野菜ヲタベ アラユルコトヲ ジブンヲカンジョウニ入レズニ ヨクミキキシワカリ ソシテワスレズ",
+                  @"ビールを飲む"
+                  ];
+
+    [self.tableView registerClass:[UITableViewCell class]
+           forCellReuseIdentifier:@"cell"];
+    self.tableView.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.todo.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    cell.textLabel.text = self.todo[indexPath.row];
+    return cell;
 }
 
 @end
