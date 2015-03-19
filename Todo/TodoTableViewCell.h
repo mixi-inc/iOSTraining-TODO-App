1bd803f11a8122b33fb7e8bb610667a1d4807fb7
Yuichi.Takeda <yuichi.takeda@mixi.co.jp>
2015-03-19 20:43:52 +0900

    TodoTableViewCellを追加

---
+++ Todo/TodoTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TodoTableViewCell.h
+//  Todo
+//
+//  Created by yuichi.takeda on 3/19/15.
+//  Copyright (c) 2015 appdojo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodoTableViewCell : UITableViewCell
+
+@end
---
+++ Todo/TodoTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TodoTableViewCell.m
+//  Todo
+//
+//  Created by yuichi.takeda on 3/19/15.
+//  Copyright (c) 2015 appdojo. All rights reserved.
+//
+
+#import "TodoTableViewCell.h"
+
+@implementation TodoTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
